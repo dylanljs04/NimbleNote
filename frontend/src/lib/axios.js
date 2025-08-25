304753689cb877791a92d0b14d4f01ef8520d914
Dylan Lim <limjishen@gmail.com>
2025-08-25 20:49:17 +0100

    Fix minot bugs

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Theres no local host in prod, so we need this to be dynamic
 const BASE_URL = import.meta.env.mode === "development" ? "http://localhost:5001/api/" : "/api"
 const api = axios.create({
-    baseURL: BASE_URL;
+    baseURL: BASE_URL
 })
 
 export default api;
